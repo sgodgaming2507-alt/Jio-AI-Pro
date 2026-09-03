fe363e2f0cc590074d4b0882bfc001438687b17a
sgodgaming2507-alt <sgodgaming2507@gmail.com>
2026-09-03 18:19:19 +0530

    Update MainActivity.kt

--- app/src/main/java/org/sgod/jioai/MainActivity.kt
+++ app/src/main/java/org/sgod/jioai/MainActivity.kt
@@ -13,37 +13,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // Check for Overlay Permission
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
-            val intent = Intent(
-                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:$packageName")
-            )
-            startActivityForResult(intent, 1234)
-        } else {
-            startFloatingService()
+        // Check for Overlay Permission (SYSTEM_ALERT_WINDOW)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:$packageName")
+                )
+                startActivityForResult(intent, 1234)
+                Toast.makeText(this, "Please allow permission to display over other apps", Toast.LENGTH_LONG).show()
+                return
+            }
         }
+
+        startFloatingService()
     }
 
     private fun startFloatingService() {
-        val intent = Intent(this, FloatingService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(intent)
-        } else {
-            startService(intent)
+        try {
+            val intent = Intent(this, FloatingService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(intent)
+            } else {
+                startService(intent)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-        Toast.makeText(this, "Radhe Admin Panel Started", Toast.LENGTH_SHORT).show()
+        
+        // Minimize/Close main activity to show the floating panel smoothly over other apps
         finish()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 1234) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
-                startFloatingService()
-            } else {
-                Toast.makeText(this, "Overlay Permission is Required!", Toast.LENGTH_LONG).show()
-                finish()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (Settings.canDrawOverlays(this)) {
+                    startFloatingService()
+                } else {
+                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
             }
         }
     }
