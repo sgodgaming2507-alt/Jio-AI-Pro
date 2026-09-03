5193126f8b8663e5064f9d88575a8e56dbc9a5da
sgodgaming2507-alt <sgodgaming2507@gmail.com>
2026-09-03 11:54:36 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/org/sgod/jioai/MainActivity.kt
@@ -0,0 +1,50 @@
+package org.sgod.jioai
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Check for Overlay Permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivityForResult(intent, 1234)
+        } else {
+            startFloatingService()
+        }
+    }
+
+    private fun startFloatingService() {
+        val intent = Intent(this, FloatingService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
+        Toast.makeText(this, "Radhe Admin Panel Started", Toast.LENGTH_SHORT).show()
+        finish()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1234) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
+                startFloatingService()
+            } else {
+                Toast.makeText(this, "Overlay Permission is Required!", Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+    }
+}
